a768dda0500832ea293f9f97f999af7ad0c4e1b4
Radika Badragtiha <puturadika69@gmail.com>
2024-05-08 20:27:53 +0800

    revert commit

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="bg-slate-500 flex justify-center items-center fixed bottom-0 w-full">
-    <p class="text-white font-semibold">From Primakara Developers with love</p>
-  </div>
+  <div></div>
 </template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,25 +1,11 @@
 import AuthorTemplates from '@/views/templates/authorTemplates.vue'
 import UserTemplates from '@/views/templates/userTemplates.vue'
-import Home from '@/views/pages/home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import About from '@/views/pages/about.vue'
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: UserTemplates,
-    children: [
-      {
-        path: '/',
-        name: 'user-home',
-        component: Home
-      },
-      {
-        path: '/about',
-        name: 'about',
-        component: About
-      }
-    ]
+    component: UserTemplates
   },
   {
     path: '/dashboard',
